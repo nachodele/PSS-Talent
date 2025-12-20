553c0527cd9192efe7e054a9e4c7f4784687d295
nachodele <nachodelecea@gmail.com>
2025-12-20 14:25:38 +0100

    urls

--- Trabajo-Final/Dev-Workspace/terraform/outputs.tf
+++ Trabajo-Final/Dev-Workspace/terraform/outputs.tf
@@ -2,3 +2,17 @@ output "alb_dns_name" {
   description = "ALB DNS para PR links"
   value       = aws_lb.pr_alb.dns_name
 }
+
+output "alb_health_url" {
+  description = "Health check completo"
+  value       = "http://${aws_lb.pr_alb.dns_name}/health"
+}
+
+output "alb_version_url" {
+  description = "Version endpoint completo"
+  value       = "http://${aws_lb.pr_alb.dns_name}/version"
+}
+
+output "instance_public_ip" {
+  value = aws_instance.pr_instance.public_ip
+}
